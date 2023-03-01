3040da002e115402c7ba7ad76658aa180277dece
brunotecnqn <91470629+brunotecnqn@users.noreply.github.com>
2023-03-01 06:35:17 -0300

    Quitando archivos obsoletos

--- Vista/estructura/footer.php
+++
@@ -1,6 +0,0 @@
-</div>
-<div data-options="region:'south',border:false" style="height:50px;background:#212529;color:white;padding:10px;text-align:center;">© 2022 Copyright: PWD - Grupo Nº7</div>
-   </body>
-            </html>
-
-
