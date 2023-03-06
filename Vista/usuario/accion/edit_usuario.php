c06ffb3f085953b92042561d33c833bea14d648e
brunotecnqn <91470629+brunotecnqn@users.noreply.github.com>
2023-03-06 15:43:16 -0300

    Update edit_usuario.php

--- Vista/usuario/accion/edit_usuario.php
+++ Vista/usuario/accion/edit_usuario.php
@@ -3,26 +3,19 @@ include_once "../../../configuracion.php";
 $data = data_submitted();
 $objSesion = new Session();
 
-$idusuario =$objSesion->getUsuario()->getIdusuario();
-$data["idusuario"]=$idusuario;
+$idusuario = $objSesion->getUsuario()->getIdusuario();
+$data["idusuario"] = $idusuario;
 
-$respuesta = false;
-if (isset($data['idusuario'])){
-    $objC = new ABMUsuario();
-    $respuesta = $objC->modificacion($data);
-    
-    if (!$respuesta){
+$respuesta = $objC->modificacion($data);
 
-        $sms_error = " La accion  MODIFICACION No pudo concretarse";
-        
-    }else $respuesta =true;
-    
+if (!$respuesta) {
+
+    $retorno['errorMsg'] = "No pudo eliminarse el usuario";
 }
+
+
 $retorno['respuesta'] = $respuesta;
-if (isset($mensaje)){
-    
-    $retorno['errorMsg']=$sms_error;
-    
-}
+
+
 echo json_encode($retorno);
 ?>
